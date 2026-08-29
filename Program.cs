6db2e68eee3f6444649b77e10bd0279ffcfe7fea
Joshua Kinsman <jkinsman00@gmail.com>
2026-08-28 19:36:36 -0500

    init console output

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(value: new string('*', 68));
+        Console.WriteLine(value: new string('*', 68));
+
+        Console.WriteLine(value: "**                                                                **");
+        Console.WriteLine(value: "**   ____  _   _ ____ _____ _____ __  __   ___ _   _ _____ ___    **");
+        Console.WriteLine(value: "**  / ___|| | | / ___|_   _| ____|  \\/  | |_ _| \\ | |  ___/ _ \\   **");
+        Console.WriteLine(value: "**  \\___ \\| |_| \\___ \\ | | |  _| | |\\/| |  | ||  \\| | |_ | | | |  **");
+        Console.WriteLine(value: "**   ___) | |_| |___) || | | |___| |  | |  | || |\\  |  _|| |_| |  **");
+        Console.WriteLine(value: "**  |____/ \\___/|____/ |_| |_____|_|  |_| |___|_| \\_|_|   \\___/   **");
+        Console.WriteLine(value: "**                                                                **");
+
+        Console.WriteLine(value: new string('*', 68));
+        Console.WriteLine(value: new string('*', 68));
+        Console.WriteLine(value: "jestyr98");
+        Console.WriteLine();
+        Console.WriteLine();
+        
+    }
+}
