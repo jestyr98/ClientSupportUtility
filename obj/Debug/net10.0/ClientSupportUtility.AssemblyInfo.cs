4ec045cfaf0e921a21effbf2306524d35f70bad6
Joshua Kinsman <jkinsman00@gmail.com>
2026-09-07 21:11:46 -0500

    assembly version update

--- obj/Debug/net10.0/ClientSupportUtility.AssemblyInfo.cs
+++ obj/Debug/net10.0/ClientSupportUtility.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSupportUtility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e8742d7b249643a0f673fbda892db8ee50a944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c832d52008efc985d4e2b42cd1b14abc6c4097")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSupportUtility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSupportUtility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
